5920ed13ef984a3441e3b9177d630eeca1ab9167
gogoonbuntu <tmddud333@naver.com>
2025-04-09 21:53:47 +0900

    기본기능 완성

--- lib/diary_list_screen.dart
+++ lib/diary_list_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:diary_app/models/diary_entry.dart';
 import 'package:diary_app/services/diary_service.dart';
 import 'package:diary_app/diary_entry_screen.dart';
@@ -35,23 +36,40 @@ class _DiaryListScreenState extends State<DiaryListScreen> {
         itemCount: _diaryEntries.length,
         itemBuilder: (context, index) {
           int actualIndex = _diaryEntries.length - 1 - index; // Correct index for reversed list
-          return ListTile(
-            title: Text(_diaryEntries[index].toString()),
-            onTap: () async {
-              final refresh = await Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => DiaryEntryScreen(diaryEntry: _diaryEntries[index])),
-              );
-              if (refresh ?? false) {
-                _loadDiaryEntries();
-              }
-            },
-            trailing: IconButton(
-              icon: const Icon(Icons.delete),
-              onPressed: () async {
-                await DiaryService().deleteDiaryEntry(_diaryEntries[index].id);
-                _loadDiaryEntries();
-              },
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey.shade300),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: ListTile(
+                title: Text(
+                  DateFormat('yyyy-MM-dd').format(_diaryEntries[index].date),
+                  style: const TextStyle(fontWeight: FontWeight.bold),
+                ),
+                subtitle: Text(
+                  _diaryEntries[index].content.split('\n').take(3).join('\n'),
+                  maxLines: 3,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                trailing: IconButton(
+                  icon: const Icon(Icons.delete),
+                  onPressed: () async {
+                    await DiaryService().deleteDiaryEntry(_diaryEntries[index].id);
+                    _loadDiaryEntries();
+                  },
+                ),
+                onTap: () async {
+                  final refresh = await Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => DiaryEntryScreen(diaryEntry: _diaryEntries[index])),
+                  );
+                  if (refresh ?? false) {
+                    _loadDiaryEntries();
+                  }
+                },
+              ),
             ),
           );
         },
